02ea12e01520f93d1797c51a19a5f2ebbd9744c9
Sebastian Post <sebastianpost@online.de>
2023-05-30 18:44:31 +0200

    use mocks for request

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { delay, Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import env from '../../../env';
 
@@ -76,6 +76,24 @@ export class WeatherService {
   constructor(private http: HttpClient) {}
 
   getWeatherByIp(): Observable<WeatherByIp> {
+    return of({
+      ip: '18.197.117.10',
+      type: 'ipv4',
+      continent_code: 'EU',
+      continent_name: 'Europe',
+      country_code: 'DE',
+      country_name: 'Germany',
+      is_eu: 'true',
+      geoname_id: 2925533,
+      city: 'Frankfurt am Main',
+      region: '',
+      lat: 50.1109,
+      lon: 8.68213,
+      tz_id: 'Europe/Berlin',
+      localtime_epoch: 1685465004,
+      localtime: '2023-05-30 18:43',
+    }).pipe(delay(1000));
+
     const params = new HttpParams().append('q', 'auto:ip');
 
     return this.http.get<WeatherByIp>(`${this.url}/ip.json`, {
@@ -87,6 +105,48 @@ export class WeatherService {
   getWeatherByLocation({
     coords,
   }: GeolocationPosition): Observable<WeatherData> {
+    return of({
+      location: {
+        name: 'Schweinfurt',
+        region: 'Bayern',
+        country: 'Germany',
+        lat: 50.06,
+        lon: 10.24,
+        tz_id: 'Europe/Berlin',
+        localtime_epoch: 1685464967,
+        localtime: '2023-05-30 18:42',
+      },
+      current: {
+        last_updated_epoch: 1685464200,
+        last_updated: '2023-05-30 18:30',
+        temp_c: 18.6,
+        temp_f: 65.5,
+        is_day: 1,
+        condition: {
+          text: 'Sunny',
+          icon: '//cdn.weatherapi.com/weather/64x64/day/113.png',
+          code: 1000,
+        },
+        wind_mph: 11.6,
+        wind_kph: 18.7,
+        wind_degree: 47,
+        wind_dir: 'NE',
+        pressure_mb: 1021,
+        pressure_in: 30.15,
+        precip_mm: 0,
+        precip_in: 0,
+        humidity: 48,
+        cloud: 6,
+        feelslike_c: 18.6,
+        feelslike_f: 65.5,
+        vis_km: 10,
+        vis_miles: 6,
+        uv: 5,
+        gust_mph: 15.7,
+        gust_kph: 25.2,
+      },
+    }).pipe(delay(1000));
+
     const coordinates = [coords.latitude, coords.longitude].toString();
     const params = new HttpParams().append('q', coordinates);
 
