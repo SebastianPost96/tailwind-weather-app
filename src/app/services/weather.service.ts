fcc82cee46eeb994e159ef216686463ca4aca004
Sebastian Post <sebastianpost@online.de>
2023-05-30 18:52:59 +0200

    refactor models

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,66 +2,8 @@ import { Injectable } from '@angular/core';
 import { delay, Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import env from '../../../env';
-
-interface WeatherData {
-  location: {
-    name: string;
-    region: string;
-    country: string;
-    lat: number;
-    lon: number;
-    tz_id: string;
-    localtime_epoch: number;
-    localtime: string;
-  };
-  current: {
-    last_updated_epoch: number;
-    last_updated: string;
-    temp_c: number;
-    temp_f: number;
-    is_day: number;
-    condition: {
-      text: string;
-      icon: string;
-      code: number;
-    };
-    wind_mph: number;
-    wind_kph: number;
-    wind_degree: number;
-    wind_dir: string;
-    pressure_mb: number;
-    pressure_in: number;
-    precip_mm: number;
-    precip_in: number;
-    humidity: number;
-    cloud: number;
-    feelslike_c: number;
-    feelslike_f: number;
-    vis_km: number;
-    vis_miles: number;
-    uv: number;
-    gust_mph: number;
-    gust_kph: number;
-  };
-}
-
-interface WeatherByIp {
-  ip: string;
-  type: string;
-  continent_code: string;
-  continent_name: string;
-  country_code: string;
-  country_name: string;
-  is_eu: string;
-  geoname_id: number;
-  city: string;
-  region: string;
-  lat: number;
-  lon: number;
-  tz_id: string;
-  localtime_epoch: number;
-  localtime: string;
-}
+import { WeatherByIp } from '../models/weather-by-ip.model';
+import { WeatherData } from '../models/weather-data.model';
 
 @Injectable({
   providedIn: 'root',
