e82407e015828442d0cdf79261ea34950b11f6fd
Sebastian Post <sebastianpost@online.de>
2023-06-03 17:14:06 +0200

    add mobile header colors

--- src/app/services/responsiveness.service.ts
+++ src/app/services/responsiveness.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { map } from 'rxjs';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { AppStateService } from './app-state.service';
 
 @Injectable({
@@ -18,7 +18,27 @@ export class ResponsivenessService {
 
   unitSystem = signal<'metric' | 'imperial'>('metric');
 
+  constructor() {
+    this.syncMobileHeaderColor();
+  }
+
   toggleUnitSystem() {
     this.unitSystem.update((val) => (val === 'metric' ? 'imperial' : 'metric'));
+  }
+
+  private syncMobileHeaderColor() {
+    toObservable(this.darkMode)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => {
+        queueMicrotask(() => {
+          const toolbar = document.getElementById('body-bg');
+          const headerElem = document.getElementById('theme-color');
+
+          if (toolbar && headerElem) {
+            const color = getComputedStyle(toolbar).backgroundColor;
+            headerElem.setAttribute('content', color);
+          }
+        });
+      });
   }
 }
