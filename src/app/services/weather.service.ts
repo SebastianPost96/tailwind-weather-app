a0254eef7b9c4ef3da890261a58499bf8f4b67ff
Sebastian Post <sebastianpost@online.de>
2023-06-03 15:39:53 +0200

    add error catching

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Observable, switchMap } from 'rxjs';
+import { catchError, Observable, of, switchMap } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import env from '../../../env';
 import { WeatherData } from '../models/weather-data.model';
 import { WeatherByIp } from '../models/weather-by-ip.model';
+import { weatherDataMock } from '../models/mocks/weather-data.mock';
+import { weatherByIpMock } from '../models/mocks/weather-by-ip.mock';
 
 @Injectable({
   providedIn: 'root',
@@ -25,6 +27,7 @@ export class WeatherService {
     });
 
     return request$.pipe(
+      catchError(() => of(weatherByIpMock)),
       switchMap(({ lat, lon }) => {
         return this.getWeatherByLocation(lat, lon);
       })
@@ -35,9 +38,11 @@ export class WeatherService {
     const coordinates = [lat, lon].toString();
     const params = new HttpParams().append('q', coordinates);
 
-    return this.http.get<WeatherData>(`${this.url}/current.json`, {
-      headers: this.headers,
-      params,
-    });
+    return this.http
+      .get<WeatherData>(`${this.url}/current.json`, {
+        headers: this.headers,
+        params,
+      })
+      .pipe(catchError(() => of(weatherDataMock)));
   }
 }
