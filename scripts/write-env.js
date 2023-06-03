353793dc11aa10d29266a9fd51817bd5e08388fd
Sebastian Post <sebastianpost@online.de>
2023-06-03 16:39:05 +0200

    fix write path

--- scripts/write-env.js
+++ scripts/write-env.js
@@ -1,8 +1,8 @@
 import { writeFile } from "fs";
 
-const targetPath = "../env.ts";
+const targetPath = "./env.ts";
 
-const envConfigFile = process.env.ENVIRONMENT;
+const envConfigFile = "process.env.ENVIRONMENT";
 
 writeFile(targetPath, envConfigFile, "utf8", (err) => {
   if (err) {
