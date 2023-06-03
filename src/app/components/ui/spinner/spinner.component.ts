5d3c9fa9db2811d8a684bfbb59e8fd6c93875bc3
Sebastian Post <sebastianpost@online.de>
2023-06-03 19:48:03 +0200

    use ngsrc

--- src/app/components/ui/spinner/spinner.component.ts
+++ src/app/components/ui/spinner/spinner.component.ts
@@ -1,12 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-spinner',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './spinner.component.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SpinnerComponent {}
+export class SpinnerComponent {
+}
