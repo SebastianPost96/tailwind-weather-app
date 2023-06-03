1aec8e333cb71abdaa97354422c37c4a5798f199
Sebastian Post <sebastianpost@online.de>
2023-06-03 16:33:39 +0200

    add env write script

---
+++ scripts/write-env.js
@@ -0,0 +1,11 @@
+import { writeFile } from "fs";
+
+const targetPath = "../env.ts";
+
+const envConfigFile = process.env.ENVIRONMENT;
+
+writeFile(targetPath, envConfigFile, "utf8", (err) => {
+  if (err) {
+    return console.log(err);
+  }
+});
