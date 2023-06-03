f263503a1e2a89dcf60857cb4f81a5596b306594
Sebastian Post <sebastianpost@online.de>
2023-06-03 14:59:29 +0200

    add info to about component

--- src/app/components/feature/about/about.component.ts
+++ src/app/components/feature/about/about.component.ts
@@ -6,10 +6,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './about.component.html',
-  styles: [
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
